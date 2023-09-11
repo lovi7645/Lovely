41bdc0724399d9e9768142d88beaae6e56c014a6
KILLSWITCH44 <144653081+KILLSWITCH44@users.noreply.github.com>
2023-09-11 12:35:40 +0530

    Update config.py
    Signed-off-by: KILLSWITCH44 <144653081+KILLSWITCH44@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/loveguruoo")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RICH_OF_HEART")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/8d67de70cace4014032de.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/da8962eb41ab43a6d3511.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
