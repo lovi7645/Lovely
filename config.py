947df04193cacadda4b5137f75d07652ade6e4b1
lovi7645 <143011723+lovi7645@users.noreply.github.com>
2023-09-13 19:23:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KILLSWITCH44/AnonXMusic",
+    "https://github.com/lovi7645/Lovely",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
